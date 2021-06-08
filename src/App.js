d5a0584171912b386118dfee91715a1c1edd674f
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-08 16:37:08 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -7,39 +7,33 @@ class App extends Component{
   constructor(){
     super()
     this.state = {
-      monsters: [
-        {
-          name: 'frank', 
-          id: '1dwq'
-        },
-        {
-          name: 'paul',
-          id: 'csdw21'
-        },
-        {
-          name: 'john',
-          id: '123213adw'
-        }
-      ]
+      monsters: []
     }
+  }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+  
+  render(){
+    return (
+      <div className="App">
+      {
+        this.state.monsters.map(monster => 
+        <h1 key={monster.id}> {monster.name}</h1>)
+      }
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <button onClick = {() => this.setState({string: 'hello world'})}>
+            change text
+          </button>
+        </header>
+      </div>
+    );
   }
 }
 
-render(){
-  return (
-    <div className="App">
-    {
-      this.state.monsters.map(monster => 
-      <h1>{monster.name}</h1>)
-    }
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <button onClick = {() => this.setState({string: 'hello world'})}>
-          change text
-        </button>
-      </header>
-    </div>
-  );
-}
 export default App;
